dc5361c0b63c06687833445f8b55487bf3107304
khan-abd <67543984+khan-abd@users.noreply.github.com>
2023-03-21 11:41:12 +0530

    Update binary-updation.py

--- binary-updation.py
+++ binary-updation.py
@@ -1,56 +1,61 @@
 import pickle
-d={}
-with open("product.dat","wb")as f:
-    ch='y'
-    while ch=='y':
-        code=input("Enter the product code")
-        desc=input("Enter the product description")
-        sto=input("Enter the stock")
-        d["prod_code"]=code
-        d["prod_desc"]=desc
-        d["stock"]=sto
-        pickle.dump(d,f)
-        ch=input("Do u want to continue(y/n)?")
-    f.close()
-def prod():
-   
-    d={}
+with open ("emplo.dat", "wb") as f:
+    pickle.dump(["name", "empid", "salary"], f)   
+    while True:
+        name=input("Enter name: ")
+        empid=int(input("Enter id: "))
+        salary=int(input("Enter salary: "))
+        pickle.dump([name, empid, salary], f)
+        ch=input("Continue? ")    
+        if ch.lower()=="n":
+            break
+def search(searchid):
     found=False
-    fin=open("product.dat","rb+")
-    STO=input("Enter the code to be updated")
-    ST=input("Enter the new stock")
-    try:
-        while True:
-            rpos=fin.tell()
-            d=pickle.load(fin)
-            if d["stock"]==STO:
-                d["stock"]=ST
-                fin.seek(rpos)
-                pickle.dump(d,fin)
-                found=True
-                break
-    except EOFError:
-        if found==False:
-            print("Sorry stock not matched")
-        else:
-            print("Update successful")
-        fin.close()
+    with open ("emplo.dat", "rb") as f1: 
+        
+        try:
+            while True:
+                x=pickle.load(f1)
+                if x[1]==searchid:
+                    found=True
+                    print("Record required: ", x)
 
-def readinfo():
-    fout=open("product.dat","rb")
+        except EOFError:
+            if found==False:
+                print("Record not found")
+
+def update(updateid):
+    found=False
+    f2=open ("emplo.dat", "rb+")
     try:
         while True:
-            stu=pickle.load(fout)
-            print(stu)
-            found=True
+            rpos=f2.tell()
+            x=pickle.load(f2)
+            if list(x)[1]==updateid:
+                found=True
+                change=int(input("Enter new salary: "))
+                x[2]=change
+                f2.seek(rpos)
+                pickle.dump(x, f2)
+                print("Record updated!. See: ", x)
+
     except EOFError:
-        if found==False:
-            print("Unsuccessful")
-        else:
-            print("Success")
-    fout.close()
-prod()
-readinfo()
-            
-            
-    
+            if found==False:
+                print("Record not found")
+
+while True:
+    menu=int(input('''
+    1. Search
+    2. Update
+    3. Exit
+    '''))
+
+    if menu==1:
+        c=int(input("Enter empid to be searched: "))
+        search(c)
+    elif menu==2:
+        c=int(input("Enter empid to be updated: "))
+        update(c)
+    elif menu==3:
+        break
+
